5b9b3f1d39ac9df359bee9bad09fb7c29d20aa09
Ricardo Andrés Silva Torres <ra.silva@uniandes.edu.co>
2023-12-03 19:28:45 -0500

    Update r_script.R

--- Scripts/r_script.R
+++ Scripts/r_script.R
@@ -136,21 +136,21 @@ skim(test_personas)
 # La columna id identifica el hogar y orden es la identif<icación de persona
 # id    - identificador
 # Orden - Llave de persona
-# P6020 - Dummy, Sexo (1 hombre, 2 mujer)
-# P6040 - Años cumplidos
-# P6050 - (Factor) Parentesco con jefe
-# P6090 - Afiliado/cotizante/beneficiario seg salud
-# P6100 - (Factor) Regimenes de seg social salud
-# P6210 - (Factor) Nivel educ mas alto (a, b, c, d, e, f, g)
-# P6240 - (Factor) Actividad ocupada semana pasada
-# P6426 - Tiempo en trabajo
-# P6430 - (Factor) Tipo posicion en ocupacion primera actividad
-# P6800 - Horas a la semana trabajadas normalmente
-# P6920 - (Factor) Cotizando en fondo de pensiones (1 si, 2 no, 3 pensionado)
-# P7040 - Dummy, Tenia otro trabajo o negocio en semana pasada ademas de oc principal? (1 si, 2 no)
-# P7090 - Dummy, Ademas de horas trabajadas, quiere trabajar mas? (1 si, 2 no)
-# P7495 - Dummy, Pagos de arriendos o pensiones (1 si, 2 no)
-# P7505 - Dummy, recibir dinero varias fuentes (1 si, 2 no)
+# Sexo  -       P6020 - Dummy, Sexo (1 hombre, 2 mujer)
+# Edad  -       P6040 - Años cumplidos
+# Parentesco -  P6050 - (Factor) Parentesco con jefe
+# Estado_SegS - P6090 - Afiliado/cotizante/beneficiario seg salud
+# Reg_Salud -   P6100 - (Factor) Regimenes de seg social salud
+# NivelEduc -   P6210 - (Factor) Nivel educ mas alto (a, b, c, d, e, f, g)
+# Actividad -   P6240 - (Factor) Actividad ocupada semana pasada
+# Tiempo_Trabajo - P6426 - Tiempo en trabajo
+# Posicion_Ocu -   P6430 - (Factor) Tipo posicion en ocupacion primera actividad
+# Horas_trabajo -  P6800 - Horas a la semana trabajadas normalmente
+# Pension -      P6920 - (Factor) Cotizando en fondo de pensiones (1 si, 2 no, 3 pensionado)
+# Otro_trabajo - P7040 - Dummy, Tenia otro trabajo o negocio en semana pasada ademas de oc principal? (1 si, 2 no)
+# Mas_horas_Trabajo - P7090 - Dummy, Ademas de horas trabajadas, quiere trabajar mas? (1 si, 2 no)
+# Pago_arr_pen - P7495 - Dummy, Pagos de arriendos o pensiones (1 si, 2 no)
+# Pago_otros -   P7505 - Dummy, recibir dinero varias fuentes (1 si, 2 no)
 # Pet   - Dummy, Poblacion en edad de trabajar (1 si, 0 no)
 # Oc    - Dummy, Ocupado (1 si)
 # Des   - Dummy, Desocupado (1 si)
@@ -160,15 +160,188 @@ skim(test_personas)
 # Ingtot - TRAIN Ingreso total
 skim(train_personas)
 
+# rename(data, new = old)
+test_personas <- test_personas %>% rename(Sexo = P6020,
+                                          Edad = P6040,
+                                          Parentesco = P6050,
+                                          Estado_SegS = P6090,
+                                          Reg_Salud = P6100,
+                                          NivelEduc = P6210,
+                                          Actividad = P6240,
+                                          Tiempo_Trabajo = P6426,
+                                          Posicion_Ocu = P6430,
+                                          Horas_trabajo = P6800,
+                                          Pension = P6920,
+                                          Otro_trabajo = P7040,
+                                          Mas_horas_Trabajo = P7090,
+                                          Pago_arr_pen = P7495,
+                                          Pago_otros = P7505)
+
+# rename(data, new = old)
+train_personas <- train_personas %>% rename(Sexo = P6020,
+                                            Edad = P6040,
+                                            Parentesco = P6050,
+                                            Estado_SegS = P6090,
+                                            NivelEduc = P6210,
+                                            Reg_Salud = P6100,
+                                            Actividad = P6240,
+                                            Tiempo_Trabajo = P6426,
+                                            Posicion_Ocu = P6430,
+                                            Horas_trabajo = P6800,
+                                            Pension = P6920,
+                                            Otro_trabajo = P7040,
+                                            Mas_horas_Trabajo = P7090,
+                                            Pago_arr_pen = P7495,
+                                            Pago_otros = P7505)
+
+unique(test_personas$Parentesco)
+unique(test_personas$Reg_Salud)
+unique(test_personas$NivelEduc)
+unique(test_personas$Actividad)
+unique(test_personas$Posicion_Ocu)
+unique(test_personas$Pension)
+unique(test_personas$Otro_trabajo)
+unique(test_personas$Mas_horas_Trabajo)
+unique(test_personas$Pago_arr_pen)
+unique(test_personas$Pago_otros)
+
+test_personas <- test_personas %>% mutate(EstadoVivienda = case_when(Sexo == 1 ~ "hombre",
+                                                                     Sexo == 2 ~ "mujer"),
+                                          Parentesco = case_when(Parentesco == 1 ~ "Jefe",
+                                                                 Parentesco == 2 ~ "Pareja",
+                                                                 Parentesco == 3 ~ "Hijo",
+                                                                 Parentesco == 4 ~ "Hijastro",
+                                                                 Parentesco == 5 ~ "Nieto",
+                                                                 Parentesco == 6 ~ "Otro pariente",
+                                                                 Parentesco == 7 ~ "Empleado",
+                                                                 Parentesco == 8 ~ "Pensionista",
+                                                                 Parentesco == 9 ~ "Otro no pariente"),
+                                          
+                                          Estado_SegS = case_when(Estado_SegS == 1 ~ "si",
+                                                                  Estado_SegS == 2 ~ "no",
+                                                                  Estado_SegS == 9 ~ "No sabe"),
+                                          
+                                          Reg_Salud = case_when(Reg_Salud == 1 ~ "Contributivo",
+                                                               Reg_Salud == 2 ~ "Especial",
+                                                               Reg_Salud == 3 ~ "Subsidiado",
+                                                               Reg_Salud == 9 ~ "No sabe"),
+                                          
+                                          NivelEduc = case_when(NivelEduc == 1 ~ "Ninguno",
+                                                                NivelEduc == 2 ~ "Preescolar",
+                                                                NivelEduc == 3 ~ "BasicaPrimaria",
+                                                                NivelEduc == 4 ~ "BasicaSecundaria",
+                                                                NivelEduc == 5 ~ "Media",
+                                                                NivelEduc == 6 ~ "Superior",
+                                                                NivelEduc == 9 ~ "No sabe"),
+                                          
+                                          Actividad = case_when(NivelEduc == 1 ~ "Trabajando",
+                                                                NivelEduc == 2 ~ "Buscando",
+                                                                NivelEduc == 3 ~ "Estudiando",
+                                                                NivelEduc == 4 ~ "Oficios",
+                                                                NivelEduc == 5 ~ "Incapacitado",
+                                                                NivelEduc == 6 ~ "Otro"),
+                                          
+                                          Posicion_Ocu = case_when(Posicion_Ocu == 1 ~ "Obrero Particular",
+                                                                   Posicion_Ocu == 2 ~ "Obrero Gobierno",
+                                                                   Posicion_Ocu == 3 ~ "Domestico",
+                                                                   Posicion_Ocu == 4 ~ "Propio",
+                                                                   Posicion_Ocu == 5 ~ "Empleador",
+                                                                   Posicion_Ocu == 6 ~ "Familiar",
+                                                                   Posicion_Ocu == 9 ~ "Otro"),
+                                          
+                                          Pension = case_when(Pension == 1 ~ "Si",
+                                                              Pension == 2 ~ "No",
+                                                              Pension == 3 ~ "Pensionado"),
+                                          
+                                          Otro_trabajo = case_when(Otro_trabajo == 1 ~ "Si",
+                                                                   Otro_trabajo == 2 ~ "No"),
+                                          
+                                          Mas_horas_Trabajo = case_when(Mas_horas_Trabajo == 1 ~ "Si",
+                                                                        Mas_horas_Trabajo == 2 ~ "No"),
+                                          
+                                          Pago_arr_pen = case_when(Pago_arr_pen == 1 ~ "Si",
+                                                                   Pago_arr_pen == 2 ~ "No"),
+                                          
+                                          Pago_otros = case_when(Pago_otros == 1 ~ "Si",
+                                                                 Pago_otros == 2 ~ "No")
+                                          )
+                                                                 
+
+
+train_personas <- train_personas %>% mutate(EstadoVivienda = case_when(Sexo == 1 ~ "hombre",
+                                                                     Sexo == 2 ~ "mujer"),
+                                          Parentesco = case_when(Parentesco == 1 ~ "Jefe",
+                                                                 Parentesco == 2 ~ "Pareja",
+                                                                 Parentesco == 3 ~ "Hijo",
+                                                                 Parentesco == 4 ~ "Hijastro",
+                                                                 Parentesco == 5 ~ "Nieto",
+                                                                 Parentesco == 6 ~ "Otro pariente",
+                                                                 Parentesco == 7 ~ "Empleado",
+                                                                 Parentesco == 8 ~ "Pensionista",
+                                                                 Parentesco == 9 ~ "Otro no pariente"),
+                                          
+                                          Estado_SegS = case_when(Estado_SegS == 1 ~ "si",
+                                                                  Estado_SegS == 2 ~ "no",
+                                                                  Estado_SegS == 9 ~ "No sabe"),
+                                          
+                                          Reg_Salud = case_when(Reg_Salud == 1 ~ "Contributivo",
+                                                                Reg_Salud == 2 ~ "Especial",
+                                                                Reg_Salud == 3 ~ "Subsidiado",
+                                                                Reg_Salud == 9 ~ "No sabe"),
+                                          
+                                          NivelEduc = case_when(NivelEduc == 1 ~ "Ninguno",
+                                                                NivelEduc == 2 ~ "Preescolar",
+                                                                NivelEduc == 3 ~ "BasicaPrimaria",
+                                                                NivelEduc == 4 ~ "BasicaSecundaria",
+                                                                NivelEduc == 5 ~ "Media",
+                                                                NivelEduc == 6 ~ "Superior",
+                                                                NivelEduc == 9 ~ "No sabe"),
+                                          
+                                          Actividad = case_when(NivelEduc == 1 ~ "Trabajando",
+                                                                NivelEduc == 2 ~ "Buscando",
+                                                                NivelEduc == 3 ~ "Estudiando",
+                                                                NivelEduc == 4 ~ "Oficios",
+                                                                NivelEduc == 5 ~ "Incapacitado",
+                                                                NivelEduc == 6 ~ "Otro"),
+                                          
+                                          Posicion_Ocu = case_when(Posicion_Ocu == 1 ~ "Obrero Particular",
+                                                                   Posicion_Ocu == 2 ~ "Obrero Gobierno",
+                                                                   Posicion_Ocu == 3 ~ "Domestico",
+                                                                   Posicion_Ocu == 4 ~ "Propio",
+                                                                   Posicion_Ocu == 5 ~ "Empleador",
+                                                                   Posicion_Ocu == 6 ~ "Familiar",
+                                                                   Posicion_Ocu == 9 ~ "Otro"),
+                                          
+                                          Pension = case_when(Pension == 1 ~ "Si",
+                                                              Pension == 2 ~ "No",
+                                                              Pension == 3 ~ "Pensionado"),
+                                          
+                                          Otro_trabajo = case_when(Otro_trabajo == 1 ~ "Si",
+                                                                   Otro_trabajo == 2 ~ "No"),
+                                          
+                                          Mas_horas_Trabajo = case_when(Mas_horas_Trabajo == 1 ~ "Si",
+                                                                        Mas_horas_Trabajo == 2 ~ "No"),
+                                          
+                                          Pago_arr_pen = case_when(Pago_arr_pen == 1 ~ "Si",
+                                                                   Pago_arr_pen == 2 ~ "No"),
+                                          
+                                          Pago_otros = case_when(Pago_otros == 1 ~ "Si",
+                                                                 Pago_otros == 2 ~ "No"))    
+                                                                     
 
 # Definimos las variables categóricas para personas
-variables_categoricas_personas <- c("P6020",
-                                   "P6050",
-                                   "P6100",
-                                   "P6210",
-                                   "P6240",
-                                   "P6430",
-                                   "P6920",
+variables_categoricas_personas <- c("Sexo",
+                                   "Parentesco",
+                                   "Estado_SegS",
+                                   "NivelEduc",
+                                   "Reg_Salud",
+                                   "Actividad",
+                                   "Posicion_Ocu",
+                                   "Pension",
+                                   "Otro_trabajo",
+                                   "Mas_horas_Trabajo",
+                                   "Pago_arr_pen",
+                                   "Pago_otros",
                                    "Depto")
 
 train_personas  <- train_personas %>% mutate_at(variables_categoricas_personas, as.factor)
